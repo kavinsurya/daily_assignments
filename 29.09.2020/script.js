fe4256c3f70dec2019d23e1140ff2155df662033
kavinsurya <kavinsurya002@gmail.com>
2020-10-01 22:46:00 +0530

    Call back

---
+++ 29.09.2020/script.js
@@ -0,0 +1,34 @@
+setTimeout(function(){
+    document.getElementById('time').innerText = 10;
+    setTimeout(function(){
+        document.getElementById('time').innerText = 9;
+        setTimeout(function(){
+            document.getElementById('time').innerText = 8;
+            setTimeout(function(){
+                document.getElementById('time').innerText = 7;
+                setTimeout(function(){
+                    document.getElementById('time').innerText = 6;
+                    setTimeout(function(){
+                        document.getElementById('time').innerText = 5;
+                        setTimeout(function(){
+                            document.getElementById('time').innerText = 4;
+                            setTimeout(function(){
+                                document.getElementById('time').innerText = 3;
+                                setTimeout(function(){
+                                    document.getElementById('time').innerText = 2;
+                                    setTimeout(function(){
+                                        document.getElementById('time').innerText = 1;
+                                        setTimeout(function(){
+                                            document.getElementById('div').innerHTML = '';
+                                            document.getElementById('message').innerHTML = 'Happy Independence Day';
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
