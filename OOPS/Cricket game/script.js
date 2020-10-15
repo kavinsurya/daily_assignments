9e6c82250d5091f8d483d8d2ab3c7e7dc7347f25
kavinsurya <kavinsurya002@gmail.com>
2020-10-15 10:23:54 +0530

    Cricket game

---
+++ OOPS/Cricket game/script.js
@@ -0,0 +1,161 @@
+var Player = /** @class */ (function () {
+    function Player(name) {
+        this.ballscore = [];
+        this.playerName = name;
+        this.id = parseInt(name.split('-')[1]);
+        this.isOut = false;
+        this.ballcount = 1;
+    }
+    return Player;
+}());
+var Team = /** @class */ (function () {
+    function Team(name) {
+        this.player = [];
+        this.teamid = parseInt(name.split('-')[1]);
+        this.teamName = name;
+        for (var i = 1; i < 11; i++) {
+            this.player.push(new Player("player-" + i));
+        }
+    }
+    return Team;
+}());
+var Game = /** @class */ (function () {
+    function Game() {
+        this.team = [new Team("team-1"), new Team("team-2")];
+        this.currTeam = this.team[0];
+    }
+    Game.prototype.gameStart = function () {
+        document.getElementById('hit-2').setAttribute('disabled', 'true');
+        document.getElementById('result').setAttribute('disabled', 'true');
+    };
+    Game.prototype.scoreGenerator = function () {
+        return Math.floor(Math.random() * 7);
+    };
+    Game.prototype.switchTeam = function (teamno) {
+        this.team[teamno - 1] = this.currTeam;
+        this.currTeam = this.team[teamno];
+    };
+    Game.prototype.getPlayertotscore = function (pnum) {
+        var totscore = 0;
+        var arr = this.currTeam.player[pnum - 1].ballscore;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].score > 0) {
+                totscore += arr[i].score;
+            }
+        }
+        this.currTeam.player[pnum - 1].playerscore = totscore;
+        return totscore;
+    };
+    Game.prototype.getTeamScore = function () {
+        var teamscore = 0;
+        for (var i = 1; i <= this.currTeam.player.length; i++) {
+            teamscore += parseInt(document.getElementById("t" + this.currTeam.teamid + "p" + i + "-total").innerHTML);
+        }
+        this.currTeam.teamscore = teamscore;
+        return teamscore;
+    };
+    Game.prototype.winner = function () {
+        var wonby = document.getElementById('wonby');
+        var winteam;
+        if (this.team[0].teamscore > this.team[1].teamscore) {
+            wonby.innerHTML = 'Team-' + this.team[0].teamid;
+            winteam = this.team[0];
+        }
+        else {
+            wonby.innerHTML = 'Team-' + this.team[1].teamid;
+            winteam = this.team[1];
+        }
+        this.mom(winteam);
+    };
+    Game.prototype.mom = function (winteam) {
+        var highscorer = this.getHighscorer(winteam);
+        var mom = document.getElementById('mom');
+        mom.innerHTML = 'Player-' + highscorer.player.toString() + ' of Team-' + winteam.teamid;
+    };
+    Game.prototype.getHighscorer = function (teamno) {
+        var highscorer1 = { player: 0, score: 0 };
+        for (var i = 0; i < teamno.player.length; i++) {
+            if (teamno.player[i].playerscore > highscorer1.score) {
+                highscorer1.player = teamno.player[i].id;
+                highscorer1.score = teamno.player[i].playerscore;
+            }
+        }
+        return highscorer1;
+    };
+    return Game;
+}());
+// creating new game
+var game = new Game();
+game.gameStart();
+var currteamid = game.currTeam.teamid;
+var playerNumber = 0;
+var ballNumber = 1;
+// button click for hit 1
+document.getElementById("hit-1").addEventListener('click', function () {
+    updateballscore();
+});
+//button click for hit 2
+document.getElementById("hit-2").addEventListener('click', function () {
+    updateballscore();
+});
+//To generate result
+document.getElementById("result").addEventListener('click', function () {
+    game.winner();
+});
+//function to update balls and scores of a player
+function updateballscore() {
+    if (ballNumber <= 6 && playerNumber < 10 && !game.currTeam.player[playerNumber].isOut) {
+        var td = document.getElementById("t" + currteamid + "p" + (playerNumber + 1) + "b" + ballNumber);
+        var playerscore = game.scoreGenerator();
+        td.innerHTML = playerscore.toString();
+        var obj = { ball: ballNumber, score: playerscore };
+        game.currTeam.player[playerNumber + 1 - 1].ballscore.push(obj);
+        ballNumber++;
+        if (playerscore == 0) {
+            game.currTeam.player[playerNumber].isOut = true;
+            printscore();
+        }
+    }
+    else {
+        if (playerNumber > 9) {
+            switchteam();
+        }
+        else {
+            printscore();
+        }
+    }
+}
+//Switching teams
+function switchteam() {
+    var pscore = 0;
+    if (playerNumber < 9) {
+        pscore = game.getPlayertotscore(playerNumber);
+    }
+    if (currteamid === 1) {
+        document.getElementById("score-" + currteamid).innerHTML = playerNumber < 9 ? pscore.toString() : game.getTeamScore().toString();
+        //disabling hit-1 button and enabling hit-2 button
+        document.getElementById('hit-2').removeAttribute('disabled');
+        document.getElementById('hit-1').setAttribute('disabled', 'true');
+        game.switchTeam(currteamid);
+        currteamid = game.currTeam.teamid;
+        playerNumber = 0;
+        ballNumber = 1;
+    }
+    else {
+        document.getElementById("score-" + currteamid).innerHTML = playerNumber < 9 ? pscore.toString() : game.getTeamScore().toString();
+        document.getElementById('hit-2').setAttribute('disabled', 'true');
+        document.getElementById('hit-1').setAttribute('disabled', 'true');
+        document.getElementById('result').removeAttribute('disabled');
+    }
+}
+//To print player's score 
+function printscore() {
+    var total = document.getElementById("t" + currteamid + "p" + (playerNumber + 1) + "-total");
+    var playertot = game.getPlayertotscore(playerNumber + 1);
+    total.innerHTML = playertot.toString();
+    ballNumber = 1;
+    playerNumber++;
+}
+function reload() {
+    window.location.reload();
+}
---
+++ OOPS/Cricket game/script.ts
@@ -0,0 +1,224 @@
+interface scorePerBall {
+    ball: number;
+    score: number;
+}
+
+class Player {
+    id: number;
+    playerName: string;
+    ballscore: Array<scorePerBall> = [];
+    playerscore: number;
+    ballcount: number;
+    isOut: boolean;
+    constructor(name) {
+        this.playerName = name;
+        this.id = parseInt(name.split('-')[1])
+        this.isOut = false;
+        this.ballcount = 1;
+    }
+
+
+}
+
+class Team {
+    teamid: number;
+    teamName: string;
+    player: Array<Player> = [];
+    teamscore: number;
+
+    constructor(name) {
+        this.teamid = parseInt(name.split('-')[1])
+        this.teamName = name;
+        for (let i = 1; i < 11; i++) {
+            this.player.push(new Player(`player-${i}`));
+        }
+    }
+}
+
+
+class Game {
+    team: [Team, Team];
+    currTeam: Team;
+    winningTeam;
+    manofmatch;
+
+    constructor() {
+        this.team = [new Team(`team-1`), new Team(`team-2`)];
+        this.currTeam = this.team[0];
+    }
+
+    gameStart() {
+        document.getElementById('hit-2').setAttribute('disabled', 'true');
+        document.getElementById('result').setAttribute('disabled', 'true');
+    }
+
+    scoreGenerator() {
+        return Math.floor(Math.random() * 7)
+    }
+
+    switchTeam(teamno) {
+        this.team[teamno - 1] = this.currTeam;
+        this.currTeam = this.team[teamno];
+    }
+
+    getPlayertotscore(pnum) {
+        let totscore = 0;
+        let arr = this.currTeam.player[pnum - 1].ballscore;
+        for (let i = 0; i < arr.length; i++) {
+          if(arr[i].score >0){
+            totscore += arr[i].score;
+          }
+            
+        }
+        this.currTeam.player[pnum-1].playerscore = totscore;
+        return totscore;
+    }
+
+    getTeamScore() {
+        let teamscore = 0;
+        for (let i = 1; i <= this.currTeam.player.length ; i++) {
+            teamscore += parseInt(document.getElementById(`t${this.currTeam.teamid}p${i}-total`).innerHTML);
+        }
+        this.currTeam.teamscore = teamscore;
+
+        return teamscore;
+    }
+
+    winner() {
+        let wonby = document.getElementById('wonby');
+        let winteam;
+        if (this.team[0].teamscore > this.team[1].teamscore) {
+            wonby.innerHTML = 'Team-'+this.team[0].teamid;
+            winteam = this.team[0];
+        } else {
+            wonby.innerHTML ='Team-'+this.team[1].teamid;
+            winteam = this.team[1];
+        }
+        this.mom(winteam);
+    }
+
+
+    mom(winteam) {
+        let highscorer = this.getHighscorer(winteam);
+        let mom = document.getElementById('mom');
+        mom.innerHTML = 'Player-' + highscorer.player.toString() + ' of Team-' + winteam.teamid;
+    }
+
+    getHighscorer(teamno) {
+        let highscorer1 = { player: 0, score: 0 }
+            for (let i = 0; i < teamno.player.length; i++) {
+                if (teamno.player[i].playerscore > highscorer1.score) {
+                    highscorer1.player = teamno.player[i].id;
+                    highscorer1.score = teamno.player[i].playerscore;
+                }
+            }
+        return highscorer1;
+
+    }
+}
+
+
+// creating new game
+let game = new Game();
+game.gameStart();
+
+
+let currteamid = game.currTeam.teamid;
+let playerNumber = 0;
+let ballNumber = 1;
+
+// button click for hit 1
+document.getElementById(`hit-1`).addEventListener('click', function () {
+    updateballscore();
+})
+
+//button click for hit 2
+document.getElementById(`hit-2`).addEventListener('click', function () {
+    updateballscore();
+
+})
+
+//To generate result
+document.getElementById(`result`).addEventListener('click', function () {
+
+    game.winner();
+
+})
+
+
+//function to update balls and scores of a player
+
+function updateballscore() {
+    if (ballNumber <= 6 && playerNumber < 10 && !game.currTeam.player[playerNumber].isOut) {
+        let td = <HTMLTableCellElement>document.getElementById(`t${currteamid}p${playerNumber + 1}b${ballNumber}`);
+
+      
+        let playerscore = game.scoreGenerator()
+        td.innerHTML = playerscore.toString();
+        let obj = { ball: ballNumber, score: playerscore };
+        game.currTeam.player[playerNumber + 1 - 1].ballscore.push(obj);
+        ballNumber++;
+
+     
+        if (playerscore == 0) {
+            game.currTeam.player[playerNumber].isOut = true;
+            printscore();
+        }
+
+    }
+    else {
+      
+        if (playerNumber > 9) {
+            switchteam();       
+        }
+      else{
+       
+            printscore();
+      }
+    }
+}
+
+
+//Switching teams
+function switchteam(){
+    let pscore = 0;
+   
+
+    if(playerNumber<9){
+      
+        pscore = game.getPlayertotscore(playerNumber);
+    }
+    if (currteamid === 1) {
+        document.getElementById(`score-${currteamid}`).innerHTML = playerNumber<9 ? pscore.toString() : game.getTeamScore().toString();
+        //disabling hit-1 button and enabling hit-2 button
+        document.getElementById('hit-2').removeAttribute('disabled');
+        document.getElementById('hit-1').setAttribute('disabled', 'true');
+       
+        game.switchTeam(currteamid);
+        currteamid = game.currTeam.teamid;
+        playerNumber = 0;
+        ballNumber = 1;
+      
+    }
+    else {
+        document.getElementById(`score-${currteamid}`).innerHTML = playerNumber<9 ? pscore.toString() : game.getTeamScore().toString();
+
+      
+        document.getElementById('hit-2').setAttribute('disabled', 'true');
+        document.getElementById('hit-1').setAttribute('disabled', 'true');
+        document.getElementById('result').removeAttribute('disabled');
+    }
+}
+
+//To print player's score 
+function printscore() {
+    let total = <HTMLTableCellElement>document.getElementById(`t${currteamid}p${playerNumber + 1}-total`);
+    let playertot = game.getPlayertotscore(playerNumber + 1);
+    total.innerHTML = playertot.toString();
+    ballNumber = 1;
+    playerNumber++;
+}
+
+function reload(){
+    window.location.reload()
+}
